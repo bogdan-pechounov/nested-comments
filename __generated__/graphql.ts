d1b72caee5d247a22af314d149b364ef690a364d
Bogdan-Pechounov <119967588+Bogdan-Pechounov@users.noreply.github.com>
2023-02-03 14:46:23 -0500

    graphql codegen for apollo client

---
+++ __generated__/graphql.ts
@@ -0,0 +1,75 @@
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type PageInfo = {
+  __typename?: 'PageInfo';
+  endCursor?: Maybe<Scalars['String']>;
+  hasNextPage: Scalars['Boolean'];
+  hasPreviousPage: Scalars['Boolean'];
+  startCursor?: Maybe<Scalars['String']>;
+};
+
+export type Post = {
+  __typename?: 'Post';
+  body: Scalars['String'];
+  id: Scalars['ID'];
+  title: Scalars['String'];
+  user: User;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  ok: Scalars['Boolean'];
+  posts: QueryPostsConnection;
+};
+
+
+export type QueryPostsArgs = {
+  after?: InputMaybe<Scalars['ID']>;
+  before?: InputMaybe<Scalars['ID']>;
+  first?: InputMaybe<Scalars['Int']>;
+  last?: InputMaybe<Scalars['Int']>;
+};
+
+export type QueryPostsConnection = {
+  __typename?: 'QueryPostsConnection';
+  edges: Array<Maybe<QueryPostsConnectionEdge>>;
+  pageInfo: PageInfo;
+};
+
+export type QueryPostsConnectionEdge = {
+  __typename?: 'QueryPostsConnectionEdge';
+  cursor: Scalars['String'];
+  node: Post;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  posts: Array<Post>;
+};
+
+export type GetPostsQueryVariables = Exact<{
+  first?: InputMaybe<Scalars['Int']>;
+  after?: InputMaybe<Scalars['ID']>;
+}>;
+
+
+export type GetPostsQuery = { __typename?: 'Query', posts: { __typename?: 'QueryPostsConnection', pageInfo: { __typename?: 'PageInfo', endCursor?: string | null, hasNextPage: boolean }, edges: Array<{ __typename?: 'QueryPostsConnectionEdge', cursor: string, node: { __typename?: 'Post', title: string, id: string, user: { __typename?: 'User', name: string } } } | null> } };
+
+
+export const GetPostsDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"GetPosts"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"first"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"Int"}}},{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"after"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"ID"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"posts"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"first"},"value":{"kind":"Variable","name":{"kind":"Name","value":"first"}}},{"kind":"Argument","name":{"kind":"Name","value":"after"},"value":{"kind":"Variable","name":{"kind":"Name","value":"after"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"pageInfo"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"endCursor"}},{"kind":"Field","name":{"kind":"Name","value":"hasNextPage"}}]}},{"kind":"Field","name":{"kind":"Name","value":"edges"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"cursor"}},{"kind":"Field","name":{"kind":"Name","value":"node"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"title"}},{"kind":"Field","name":{"kind":"Name","value":"id"}},{"kind":"Field","name":{"kind":"Name","value":"user"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]}}]}}]}}]} as unknown as DocumentNode<GetPostsQuery, GetPostsQueryVariables>;
--- app/posts/PostList.tsx
+++ app/posts/PostList.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import { gql, useQuery } from '@apollo/client'
+import { gql } from '@/__generated__'
+import { useQuery } from '@apollo/client'
 import React from 'react'
 
-const PostsQuery = gql`
-  query ($first: Int, $after: ID) {
+const GET_POSTS = gql(/* GraphQL */ `
+  query GetPosts($first: Int, $after: ID) {
     posts(first: $first, after: $after) {
       pageInfo {
         endCursor
@@ -22,20 +23,21 @@ const PostsQuery = gql`
       }
     }
   }
-`
+`)
+
 export default function PostList() {
-  const { data, loading, fetchMore } = useQuery(PostsQuery, {
+  const { data, loading, fetchMore } = useQuery(GET_POSTS, {
     variables: { first: 2 },
   })
 
-  console.log(data)
   if (loading) return <div>Loading...</div>
+  if (!data) return
 
   const { endCursor, hasNextPage } = data.posts.pageInfo
 
   return (
     <div>
-      {data?.posts.edges.map(({ node }: any) => (
+      {data?.posts.edges.map(({ node }) => (
         <div key={node.id}>{node.title}</div>
       ))}
       {hasNextPage ? (
@@ -44,14 +46,6 @@ export default function PostList() {
             fetchMore({
               variables: {
                 after: endCursor,
-                //   updateQuery: (prevResult, { fetchMoreResult }) => {
-                //     fetchMoreResult.posts.edges = [
-                //       ...prevResult.posts.edges,
-                //       ...fetchMoreResult.posts.edges,
-                //     ]
-
-                //     return fetchMoreResult
-                //   },
               },
             })
           }
